d4fbd4667db9724cc377191d85844436b0e3a979
Rogério Soares <rogerio_si@yahoo.com.br>
2024-01-04 14:52:48 -0300

    agora com altura dinamica dos uavs segunda versao com graficos

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from root import root
 from calculate_results import calculate_results
 #from clustering_algoritms import clustering_algoritm
 import random
-
+import math
+import matplotlib.pyplot as plt
 
 random.seed(0)
 
@@ -42,15 +43,16 @@ for x in range(sel_n_clusters):
         centr.append(centroid)
         centro = centers[c]
 
-        # calcula o raio de cada cluster
+   # calcula o raio de cada cluster
         dis = 0
         for index, user in data.iterrows():
             dist = np.sqrt(np.sum((user - centro) ** 2))
             if dist >= dis:
                 dis = dist
     distancias.append(dist)
-    for x in distancias:
-        uav_high.append(int(x*1000))  # aqui modifica a altura
+    for raio in distancias:
+        #uav_high.append(200)  # aqui modifica a altura h=r⋅tan(θ)
+        uav_high.append(int((raio * math.tan(45))*1000))  # aqui modifica a altura h=r⋅tan(θ)
 
     print("dist", distancias)
     centr = pd.DataFrame(centr, columns=['X','Y'])
@@ -63,8 +65,6 @@ for x in range(sel_n_clusters):
     total = total + 1
 
 
-
-
 #total_users_data_rate = calculate_results(data,uav2)
 count=0
 for x in resu:
@@ -75,38 +75,63 @@ for x in resu:
    # print("UAVS ON=", x[8])
     count=count+1
 
-#plot
-# Extrair a terceira coluna dos resultados
-terceira_coluna = [x[0] for x in resu]
 
-# Criar uma lista de índices para usar como eixo x
+#######################################################################################
+#plot 01
+plt.figure()
+coluna = [x[0] for x in resu]
 indices = list(range(len(resu)))
-
-# Criar o gráfico de linhas
-plt.plot(indices, terceira_coluna, marker='o', linestyle='-', color='b', label='Users Connected')
-
-# Adicionar rótulos aos eixos
+plt.plot(indices, coluna, marker='o', linestyle='-', color='b', label='Users Connected')
 plt.xlabel('UAVBS')
 plt.ylabel('Users')
-
-# Adicionar título ao gráfico
-plt.title('UAVBS connected')
-
-# Adicionar uma legenda
 plt.legend()
+plt.title('UAVBS connected')
+#plt.show()
 
-# Exibir o gráfico
-plt.show()
+#plot 02
+plt.figure()
+coluna = [x[2] for x in resu]
+indices = list(range(len(resu)))
+plt.plot(indices, coluna, marker='o', linestyle='-', color='b', label='Users Connected')
+plt.xlabel('UAVBS')
+plt.ylabel('Data rate')
+plt.legend()
+plt.title('Average user data rate')
+#plt.show()
 
-
-#kmeans
+#kmeans 03
+plt.figure()
 plt.scatter(data["X"], data["Y"], marker='o', c='blue', label='usuario')
 plt.scatter(centr["X"], centr["Y"], marker='x', c='red', label='Sc')
 plt.xlabel('Eixo X')
 plt.ylabel('Eixo Y')
 plt.legend()
 plt.title("Kmeans Clustering")
+#plt.show()
+
+
+# kmeans color 04
+plt.figure()
+for i in range(len(centr)):
+    cluster_data = data[labels == i]
+    plt.scatter(cluster_data["X"], cluster_data["Y"], marker='o', label=f'Cluster {i}')
+plt.scatter(centr["X"], centr["Y"], marker='x', c='red', label='Centroides')
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+plt.legend()
+plt.title("Kmeans Clustering - Pontos Coloridos por Cluster")
+#plt.show()
+
+# Create a 3D scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(data["X"], data["Y"], 0, marker='o', c='blue', label='usuario')
+ax.scatter(centr["X"], centr["Y"], uav_high, marker='x', c='red', label='Sc')
+ax.set_xlabel('Eixo X')
+ax.set_ylabel('Eixo Y')
+ax.set_zlabel('Eixo Z')
+ax.legend()
+plt.title("Kmeans Clustering - 3D Visualization")
 plt.show()
 
-###########################
 
